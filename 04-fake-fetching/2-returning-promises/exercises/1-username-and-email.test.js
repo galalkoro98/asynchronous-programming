9f7697b2b003da6d2ddd86473a37d498f9af33ac
galalkoro98 <galal.koro@gmail.com>
2023-01-02 22:58:55 +0100

    exercise update

--- 04-fake-fetching/2-returning-promises/exercises/1-username-and-email.test.js
+++ 04-fake-fetching/2-returning-promises/exercises/1-username-and-email.test.js
@@ -17,6 +17,7 @@ const usernameAndEmail = (id=1) => {
     log('fetching user ' + id);
     log(`user ${id}:`, user);
     return user.id + '.' + ' ' + user.username + ', ' + user.email;
+   
   };
   const namePromise = userPromise.then(readName);
  return namePromise;
@@ -25,25 +26,21 @@ const usernameAndEmail = (id=1) => {
 // --- test function ---
 
 describe("usernameAndEmail: returns a user's name", () => {
-  it("gets user 2's name", () => {
-    return usernameAndEmail(2).then((actual) => {
-      expect(actual).toEqual('2. Antonette, Shanna@melissa.tv'); // email has been corrected 
-    });
+  it("gets user 2's name", async () => {
+    const actual = await usernameAndEmail(2);
+    expect(actual).toEqual('2. Antonette, Shanna@melissa.tv'); // email has been corrected 
   });
-  it("gets user 3's name", () => {
-    return usernameAndEmail(4).then((actual) => {
-      expect(actual).toEqual('4. Karianne, Julianne.OConner@kory.org'); // email has been corrected 
-    });
+  it("gets user 3's name", async () => {
+    const actual = await usernameAndEmail(4);
+    expect(actual).toEqual('4. Karianne, Julianne.OConner@kory.org'); // email has been corrected 
   });
-  it("gets user 4's name", () => {
-    return usernameAndEmail(7).then((actual) => {
-      expect(actual).toEqual('7. Elwyn.Skiles, Telly.Hoeger@billy.biz'); 
-    });
+  it("gets user 4's name", async () => {
+    const actual = await usernameAndEmail(7);
+    expect(actual).toEqual('7. Elwyn.Skiles, Telly.Hoeger@billy.biz');
   });
-  it("gets user 8's name", () => {
-    return usernameAndEmail(10).then((actual) => {
-      expect(actual).toEqual('10. Moriah.Stanton, Rey.Padberg@karina.biz'); // email has been corrected 
-    });
+  it("gets user 8's name", async () => {
+    const actual = await usernameAndEmail(10);
+    expect(actual).toEqual('10. Moriah.Stanton, Rey.Padberg@karina.biz'); // email has been corrected 
   });
 });
 
