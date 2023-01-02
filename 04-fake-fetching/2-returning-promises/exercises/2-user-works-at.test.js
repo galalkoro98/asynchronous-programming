ceb7a9abe5d9fecb46aa89e1e97a437abb982cad
galalkoro98 <galal.koro@gmail.com>
2023-01-02 23:48:15 +0100

    execise done

--- 04-fake-fetching/2-returning-promises/exercises/2-user-works-at.test.js
+++ 04-fake-fetching/2-returning-promises/exercises/2-user-works-at.test.js
@@ -9,30 +9,48 @@ const { log } = labeledLogger();
 /**
  *
  */
-const userWorksAt = () => {};
+const userWorksAt = (id = 1, nameOfCompany) => {
+  log('fetching user ' + id);
+  const userPromise = fetchUserById(id);
+
+  const readName = (user) => {
+    log(`user ${id}`, user);
+  
+  
+    if(user.company.name === nameOfCompany){
+      return true;
+    }
+    else{
+      return false;
+    }
+
+}
+
+
+const namePromise = userPromise.then(readName);
+return namePromise;
+
+
+};
 
 // --- test function ---
 
 describe('userWorksAt checks if a user works at a specific company', () => {
-  it('user 4 does work at Robel-Corkery', () => {
-    return userWorksAt(4, 'Robel-Corkery').then((actual) => {
-      expect(actual).toEqual(true);
-    });
+  it('user 4 does work at Robel-Corkery', async () => {
+    const actual = await userWorksAt(4, 'Robel-Corkery');
+    expect(actual).toEqual(true);
   });
-  it('user 4 does not work at Romaguera-Jacobson', () => {
-    return userWorksAt(4, 'Romaguera-Jacobson').then((actual) => {
-      expect(actual).toEqual(false);
-    });
+  it('user 4 does not work at Romaguera-Jacobson', async () => {
+    const actual = await userWorksAt(4, 'Romaguera-Jacobson');
+    expect(actual).toEqual(false);
   });
-  it('user 6 does work at Considine-Lockman', () => {
-    return userWorksAt(6, 'Considine-Lockman').then((actual) => {
-      expect(actual).toEqual(true);                                            // Correct value is true not false
-    });
+  it('user 6 does work at Considine-Lockman', async () => {
+    const actual = await userWorksAt(6, 'Considine-Lockman');
+    expect(actual).toEqual(true); // Correct value is true not false
   });
-  it('user 7 does not work at John Groups', () => {
-    return userWorksAt(7, 'John Groups').then((actual) => {
-      expect(actual).toEqual(false);
-    });
+  it('user 7 does not work at John Groups', async () => {
+    const actual = await userWorksAt(7, 'John Groups');
+    expect(actual).toEqual(false);
   });
 });
 
